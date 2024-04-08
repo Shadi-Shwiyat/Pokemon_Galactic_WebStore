e9b898d3fc53598f10ab538f36866b809dc95e01
Nomad-Rob <rfarley89@gmail.com>
2024-04-08 16:06:51 -0500

    Got JSDoc working, updated package.json to have it work and path. updated readme to show how to use

--- server/itemsRoutes.js
+++ server/itemsRoutes.js
@@ -1,9 +1,9 @@
-// Items routes
+/** Items routes */
 
 const express = require('express');
 const router = express.Router();
 const { admin } = require('../server/firebaseAdminConfig'); // Import the Firebase Admin SDK
 
 
-// Export to server/index.js
+/** Export to server/index.js */
 module.exports = router;
