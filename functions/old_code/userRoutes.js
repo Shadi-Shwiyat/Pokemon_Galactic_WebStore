ae9e9721c3e50c9ccc41001f884b40a1025dd6e8
Nomad-Rob <rfarley89@gmail.com>
2024-04-13 16:28:28 -0500

    rough draft of adding authentiation with email and password, not sure about google yet

--- functions/old_code/userRoutes.js
+++ functions/old_code/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { admin } = require('./firebaseAdminConfig'); // Import the Firebase Admin SDK
+const { admin } = require('../firebaseAdminConfig'); // Import the Firebase Admin SDK
 
 
 /** User Auth Endpionts *////////////////////////////////////////////////////////////////////////////////
