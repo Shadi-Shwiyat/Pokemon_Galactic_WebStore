9541a359f887cbf59b5ed673a16bc3ca8cf9bdb2
Shadi-Shwiyat <raz611103@gmail.com>
2024-04-15 15:28:15 -0500

    stopping point for market page, built pokemon cards and able to get data from db. need to fix gif resizing and pagination

--- app/src/components/market.jsx
+++ app/src/components/market.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
 import { Market_filter } from './filter_market';
+import { Market_cards } from './market_cards';
 
-export function Market({spriteUrl}) {
+export function Market() {
 
   return (
     <>
       <Market_filter />
+      <Market_cards />
     </>
   );
 }
