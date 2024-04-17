0b37e443199a3be31d8d267af384099d53ba8636
Nomad-Rob <rfarley89@gmail.com>
2024-04-16 19:45:32 -0500

    commet our logger stuff and testing sign in

---
+++ functions/logger.js
@@ -0,0 +1,15 @@
+// const winston = require('winston');
+
+// // Create a logger with a file and console transport
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   transports: [
+//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+//     new winston.transports.Console({
+//       format: winston.format.simple(),
+//     })
+//   ],
+// });
+
+// module.exports = logger;
